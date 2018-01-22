63ae37eef26ac46d185e905c9423c6aa2ead6e7f
FAQ_Ashraf <ashraf@faqinteractive.com.au>
2018-01-22 13:39:46 +0600

    1. Common code to convert UTC string to date object 2. Common code to convert date object to any string format 3. Common code to check connection of internet 4. Little modification on example project to test properly

--- Example/FAQUtility/ViewController.swift
+++ Example/FAQUtility/ViewController.swift
@@ -11,6 +11,7 @@ import FAQUtility
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ipTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,8 +21,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func buttonClicked(_ sender: Any) {
-        let result = FAQUtility.isEmptyString("Hello")
+        //let result = FAQUtility.isEmptyString("Hello")
+        let result1 = FAQUtility.getDate(FromUTC: ipTextField.text!)
+        let result = FAQUtility.getDateString(FromDate: result1, inFormat: "MMM d yyyy, h:mm a")
         print(result)
     }
 
--- FAQUtility/Classes/FAQUtility.swift
+++ FAQUtility/Classes/FAQUtility.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SystemConfiguration
 
 public class FAQUtility {
     
@@ -16,5 +17,104 @@ public class FAQUtility {
         } else {
             return true
         }
+    }
+    
+    public class func getDate(FromUTC inDateString: String) -> Date {
+        
+        var currentInDate = inDateString
+        
+        if (currentInDate.contains(".")) {
+            let splittedDate = currentInDate.components(separatedBy: ".")
+            
+            currentInDate = splittedDate[0]
+            
+            if splittedDate[1].contains("+") {
+                let tempSplitttedDate = splittedDate[1].components(separatedBy:"+");
+                currentInDate += "+" + tempSplitttedDate[1];
+            }
+            
+            if splittedDate[1].contains("-") {
+                let tempSplitttedDate = splittedDate[1].components(separatedBy:"-");
+                currentInDate += "-" + tempSplitttedDate[1];
+            }
+            
+            if splittedDate[1].contains("Z") {
+                currentInDate += "Z"
+            }
+            
+        }
+        
+        let dateFormatter = DateFormatter()
+        if currentInDate.count > 0 {
+            print(getDateFormatFromString(currentInDate))
+            dateFormatter.dateFormat = getDateFormatFromString(currentInDate)
+            if let currentDate = dateFormatter.date(from: currentInDate){
+                return currentDate
+            }
+        }
+        
+        return Date()
+        
+    }
+    
+    public class func getDateString(FromDate lDate:Date, inFormat lFormat: String) -> String{
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = lFormat
+        return dateFormatter.string(from: lDate)
+        
+    }
+    
+    public class  func isConnectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
+    }
+    
+    
+    // MARK:- file private methods
+    
+    fileprivate class func getDateFormatFromString(_ utcDate: String) -> String {
+        
+        switch utcDate.count {
+        case 4:
+            return "yyyy";
+        case 7:
+            return "yyyy-MM";
+        case 10:
+            return "yyyy-MM-dd";
+        default:
+            return getComplexUtcFormat(utcDate);
+        }
+        
+    }
+
+    fileprivate class func getComplexUtcFormat(_ utcDate: String) -> String {
+
+        if utcDate[utcDate.index(utcDate.startIndex, offsetBy: 16)] != ":" {
+            return "yyyy-MM-dd'T'HH:mmZ"
+        } else {
+            return "yyyy-MM-dd'T'HH:mm:ssZ"
+        }
+
     }
 }
